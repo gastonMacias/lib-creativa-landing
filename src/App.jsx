adaa1d402d73f7006bf1cbc1c5c08210d2e4cb7e
gastonMacias <gastonezequielmacias@gmail.com>
2025-06-17 17:47:10 -0300

    introduccion de componentes navbar y seccionHero

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
+import Navbar from './componentes/navbar/Navbar'
+import SeccionHero from './componentes/seccionHero/SeccionHero'
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <Navbar />
+      <SeccionHero />
     </>
   )
 }
