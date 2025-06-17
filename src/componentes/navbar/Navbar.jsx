33b40e04edea2ad88fe8f67a43a77d98be50cfd8
gastonMacias <gastonezequielmacias@gmail.com>
2025-06-17 17:47:58 -0300

    componente navbar

---
+++ src/componentes/navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import "./navbar.css"
+
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar__img">
+                    <img className="navbar__logo" src="../../../public/imagenes/gatitoFondo.ico" alt="logo" />
+                </div>
+                <div className="navbar__list">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+
+export default Navbar;
