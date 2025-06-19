fe5380f3e6f7107a95d74402920cc4aefe7db669
gastonMacias <gastonezequielmacias@gmail.com>
2025-06-19 15:23:58 -0300

    correcion componente y css de anchor

--- src/componentes/seccionHero/SeccionHero.jsx
+++ src/componentes/seccionHero/SeccionHero.jsx
@@ -8,7 +8,11 @@ const SeccionHero = () => {
                 <img className="hero__imgFondo" src="/imagenes/gatitoFondo.jpg" alt="fondo gatito" />
                 <h1 className="hero__titulo">Libreria Creativa VALKY</h1>
                 <span className="hero__span">Bienvenido a Valky: Libros, arte y creatividad en un solo lugar.</span>
-                <button className="hero__button"><a href="https://proyecto-valky.vercel.app/" target="_blank" rel="external">Ir al market</a></button>
+                <a  href="https://proyecto-valky.vercel.app/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hero__button">Ir al market
+                </a>
             </section>
         </>
     )
