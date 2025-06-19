83b4cee2ef28d365f59d1c806fed2ed14474d437
gastonMacias <gastonezequielmacias@gmail.com>
2025-06-19 14:53:13 -0300

    mejoras al css y al  componente

--- src/componentes/seccionHero/SeccionHero.jsx
+++ src/componentes/seccionHero/SeccionHero.jsx
@@ -5,10 +5,10 @@ const SeccionHero = () => {
     return(
         <>
             <section className="seccionHero">
-                <img className="hero__imgFondo" src="../../../public/imagenes/gatitoFondo.jpg" alt="" />
+                <img className="hero__imgFondo" src="/imagenes/gatitoFondo.jpg" alt="fondo gatito" />
                 <h1 className="hero__titulo">Libreria Creativa VALKY</h1>
                 <span className="hero__span">Bienvenido a Valky: Libros, arte y creatividad en un solo lugar.</span>
-                <button className="hero__button"><a href="">Link a nuestro market</a></button>
+                <button className="hero__button"><a href="">Ir al market</a></button>
             </section>
         </>
     )
