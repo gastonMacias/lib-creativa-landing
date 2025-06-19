aabcebe611faeb0cb430ba15d9b645e566ff671a
gastonMacias <gastonezequielmacias@gmail.com>
2025-06-19 15:15:16 -0300

    correccion del target del anchor

--- src/componentes/seccionHero/SeccionHero.jsx
+++ src/componentes/seccionHero/SeccionHero.jsx
@@ -2,13 +2,13 @@ import "./seccionHero.css"
 
 
 const SeccionHero = () => {
-    return(
+    return (
         <>
             <section className="seccionHero">
                 <img className="hero__imgFondo" src="/imagenes/gatitoFondo.jpg" alt="fondo gatito" />
                 <h1 className="hero__titulo">Libreria Creativa VALKY</h1>
                 <span className="hero__span">Bienvenido a Valky: Libros, arte y creatividad en un solo lugar.</span>
-                <button className="hero__button"><a href="https://proyecto-valky.vercel.app/" target_blank>Ir al market</a></button>
+                <button className="hero__button"><a href="https://proyecto-valky.vercel.app/" target="_blank" rel="external">Ir al market</a></button>
             </section>
         </>
     )
