41e457dd68208df1bac86dd081ea72ee3c42cc95
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-23 14:58:19 -0300

    componente footer iniciado y primeros estilos

---
+++ src/componentes/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import "./footer.css";
+
+
+const Footer = () => {
+    return (
+        <section className="section__footer">
+            <div className="footer__list">
+                <p className="footer__text">Pagina de aprendizaje</p>
+                <p className="footer__text">Todos los derechos reservados</p>
+                <p className="footer__text">Muchas gracias por su visita</p>
+                <a className="footer__text">Preguntas frecuentes</a>
+            </div>
+        </section>
+    )
+}
+
+
+export default Footer;
