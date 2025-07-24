534df7dae2b137347d15a6d766b27bf9ebac830b
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-24 18:49:28 -0300

    componente referencias implementado

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Footer from './componentes/footer/Footer'
 import Navbar from './componentes/navbar/Navbar'
 import Productos from './componentes/productos/Productos'
+import Referencias from './componentes/referencias/Referencias'
 import SeccionBeneficios from './componentes/seccionBeneficios/SeccionBeneficios'
 import SeccionHero from './componentes/seccionHero/SeccionHero'
 
@@ -15,6 +16,7 @@ function App() {
       <SeccionHero />
       <SeccionBeneficios />
       <Productos />
+      <Referencias />
       <Footer />
     </>
   )
