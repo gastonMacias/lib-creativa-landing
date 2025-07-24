725d0df0c4f7a08defb1ea077a8bf788f76dfa5d
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-24 19:40:03 -0300

    componente nosotros y primeros estilos

---
+++ src/componentes/nosotros/Nosotros.jsx
@@ -0,0 +1,17 @@
+import "./nosotros.css";
+
+
+const Nosotros = () => {
+    return (
+        <section className="section__nosotros">
+            <h3 className="nosotros__h3">Quienes Somos</h3>
+            <p className="nosotros__texto">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Reprehenderit mollitia iusto fuga pariatur blanditiis praesentium modi quasi rerum est 
+            provident, repellendus corrupti consectetur temporibus tempore incidunt necessitatibus 
+            repudiandae asperiores tenetur?</p>
+        </section>
+    )
+}
+
+
+export default Nosotros;
