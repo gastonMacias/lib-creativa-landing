8a654cf6362d54a4b51cdd97a22c0dcb704988cd
gastonMacias <gastonezequielmacias@gmail.com>
2025-07-24 19:31:39 -0300

    eliminado preguntas frecuentes

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -8,7 +8,6 @@ const Footer = () => {
                 <p className="footer__text">Pagina de aprendizaje</p>
                 <p className="footer__text">Todos los derechos reservados</p>
                 <p className="footer__text">Muchas gracias por su visita</p>
-                <a className="footer__text">Preguntas frecuentes</a>
             </div>
         </section>
     )
